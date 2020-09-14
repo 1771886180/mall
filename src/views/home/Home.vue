65b582e6a06450800471134b06a574fa8566dc5f
chendeyang <1771886180@qq.com>
2020-09-14 22:45:19 +0800

    轮播图的展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,28 +3,30 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper  from "@/views/home/childComps/HomeSwiper";
 import {getHomeMultiData} from "@/network/home";
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
   },
   data() {
-    return{
+    return {
       banners: [],
       recommends: []
     }
   },
   created() {
     // 1.请求多个数据
-    getHomeMultiData().then(res=>{
-      console.log(res);
+    getHomeMultiData().then(res => {
       // this.result = res;
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
